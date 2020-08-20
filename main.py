83f914e5502fd3b79a5325692317ed4d885e83dc
doriang-dev <69525163+doriang-dev@users.noreply.github.com>
2020-08-20 18:47:09 +0200

    Cleaning code

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from time import time
+from utils.img_tools import generatecolorscheme, hexify
+from colr import color
 
-from utils.img_tools import get_pixels, open_image
 
+def main():
+    # path = input('img path: ')
+    path = 'imgtest/index.jpg'
+    color_scheme = generatecolorscheme(path, 5)
 
-def main_color(img_path):
-    ti = time()
-    img = open_image(img_path)
-    pixels = get_pixels(img)
-    # count = counter(pixels)
-    # sorted_count = sorted(count.items(), key=lambda value: value[1], reverse=True)
-
-    print(pixels)
-    # print(sorted_count[0:10])
-    tf = time()
-    print('temps:', tf - ti)
+    for color_pixel in color_scheme:
+        hex_color = hexify(color_pixel)
+        print(color(hex_color, fore='ffffff', back=color_pixel))
 
 
 if __name__ == '__main__':
-    FILES = ['imgtest/index.jpg', 'imgtest/test.jpg']
-    for file in FILES:
-        print('\n', file)
-        main_color(file)
+    main()
--- utils/img_tools.py
+++ utils/img_tools.py
@@ -1,35 +1,65 @@
+from math import sqrt
+from utils.contrast import contrast
+
 from PIL import Image
+from colr import color
 
 
 def hexify(rgb_color):
-    hex_color = 'h'
+    hex_color = '#'
     for color in rgb_color:
-        hex_color += hex(color)[2:]
+        hex_component = hex(color)[2:]
+
+        if color <= 15:
+            hex_color += '0'
+
+        hex_color += hex_component
 
     return hex_color
 
 
-def open_image(path):  # TODO: test if the thumbnail is a good solution
-    size = 4, 4
-    img = Image.open(path)
-    img.thumbnail(size)
+def is_bw(rgb):
+    somme = sum(rgb)
+    if 100 <= somme <= 700:
+        return False
+    else:
+        return True
 
-    return img
+
+def open_image(path):
+    return Image.open(path)
 
 
-def get_pixels(image):
-    width, height = image.size
+def generatecolorscheme(path, nb_colors):
+    img = open_image(path)
+
+    width, height = img.size
+
+    size = width
+
+    img.thumbnail((size, size))
+
+    # step = int(size / nb_colors)
+
     pixels = []
 
-    for i in range(height):
-        for j in range(width):
-            pixel = image.getpixel((i, j))
-            somme = 0
+    for x in range(0, size, nb_colors):
+        for y in range(0, size, nb_colors):
 
-            for color in pixel:
-                somme += color
+            pixel = img.getpixel((x, y))
 
-            if 100 <= somme <= 700:
-                pixels += [hexify(pixel)]
+            if is_bw(pixel):
+                break
+
+            if len(pixels) == 0:
+                pixels.append(pixel)
+
+            else:  # TODO: contrast par rapport à toutes les couleurs de la liste
+                last = len(pixels) - 1
+                last_pixel = pixels[last]
+                contrast_color = contrast(pixel, last_pixel)
+
+                if contrast_color >= 3:
+                    pixels.append(pixel)
 
     return pixels
